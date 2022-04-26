671f63072bba10e88309923490321be96061f944
Rz0R <vpa05@mail.ru>
2022-04-26 17:15:10 +1000

    upgrade react to 18 version

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { StoreProvider } from './Store';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { StoreProvider } from "./Store";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
   <React.StrictMode>
     <StoreProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </StoreProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
