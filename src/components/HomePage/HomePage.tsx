7165084580e103f178552fc8c0c80ad078559c3a
Rz0R <vpa05@mail.ru>
2022-06-02 20:19:27 +1000

    add adaptive markup for home page

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,28 +1,32 @@
 import { Link } from 'react-router-dom';
-import { logo, artistsBtnImg, picturesBtnImg } from '../../consts/assetsPaths';
+import { artistsBtnImg, picturesBtnImg } from '../../consts/assetsPaths';
 import { CategoryType } from '../../consts/const';
 import LoadableImage from '../LoadableImage';
 
 const HomePage: React.FC = () => {
   return (
     <div className='home'>
-      <div className='logo'>
-        <img src={logo} alt='logo' />
-      </div>
+      <div className='logo'></div>
 
       <div className='quiz-menu'>
-        <Link className='quiz-menu__btn' to={`/category/${CategoryType.ARTISTS}`}>
-          <LoadableImage src={artistsBtnImg} alt='artists quiz' />
-          <h3 className='quiz-menu__btn-title'>
-            <span>artists</span> quiz
-          </h3>
-        </Link>
-        <Link className='quiz-menu__btn' to={`/category/${CategoryType.PAINTINGS}`}>
-          <LoadableImage src={picturesBtnImg} alt='artists quiz' />
-          <h3 className='quiz-menu__btn-title'>
-            <span>pictures</span> quiz
-          </h3>
-        </Link>
+        <div className='quiz-menu__wrapper'>
+          <div className='quiz-menu__item'>
+            <Link className='quiz-menu__btn' to={`/category/${CategoryType.ARTISTS}`}>
+              <LoadableImage src={artistsBtnImg} alt='artists quiz' />
+              <h3 className='quiz-menu__title'>
+                <span>artists</span> quiz
+              </h3>
+            </Link>
+          </div>
+          <div className='quiz-menu__item'>
+            <Link className='quiz-menu__btn' to={`/category/${CategoryType.PAINTINGS}`}>
+              <LoadableImage src={picturesBtnImg} alt='artists quiz' />
+              <h3 className='quiz-menu__title'>
+                <span>pictures</span> quiz
+              </h3>
+            </Link>
+          </div>
+        </div>
       </div>
       <Link className='btn btn--settings' to='/settings'>
         settings
